7df2e0072d223a22f00e4c12d98a59b4aaa50a88
CODER-007MAX <73681215+CODER-007MAX@users.noreply.github.com>
2021-03-21 22:13:26 +0530

    Update RPS-Game.py

--- RPS-Game.py
+++ RPS-Game.py
@@ -5,26 +5,21 @@ dec2=input()
 a='Rock'
 b='Paper'
 c='Scissor'
+print('Winner is : ')
 if (dec1==a and dec2==b):
-    print(play2, end=' ')
-    print('Win')
+    print(play2)
     
 elif (dec1==a and dec2==c):
-    print(play1, end=' ')
-    print('Win')
+    print(play1)
     
 elif (dec1==b and dec2==a):
-    print(play1, end=' ')
-    print('Win')
+    print(play1)
     
 elif (dec1==b and dec2==c):
-    print(play2, end=' ')
-    print('Win')
+    print(play2)
     
 elif (dec1==c and dec2==a):
-    print(play2, end=' ')
-    print('Win')
+    print(play2)
     
 else:
-    print(play1, end=' ')
-    print('Win')
+    print(play1)
