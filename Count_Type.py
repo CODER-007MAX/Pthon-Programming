e9a13a3c2403a7dd66a519f6bc263624095b6413
CODER-007MAX <73681215+CODER-007MAX@users.noreply.github.com>
2021-03-22 16:03:19 +0530

    Add files via upload

---
+++ Count_Type.py
@@ -0,0 +1,21 @@
+str=input()
+str=str.lower()
+dig=0
+alp=0
+spe=0
+vow=0
+for i in range(0, len(str)):
+    ch=str[i]
+    if ( ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u' ):
+        vow+=1
+    else:
+        alp+=1
+        
+    if ( ch>='0' and ch<='9'):
+        dig+=1
+    else:
+        spe+=1
+print("consonants are : ", alp)
+print("digits are : ", dig)
+print("special characters are : ", spe)
+print("digits are : ", dig)
